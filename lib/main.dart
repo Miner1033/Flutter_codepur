2d39ac6edfb2a9bbc05b379f0153024e9db68c59
miner <minerhossain1033@gmail.com>
2024-11-15 12:50:59 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_codepur_project/pages/login_page.dart';
+
+import 'home_page.dart';
 
 void main()
 {
@@ -13,13 +16,15 @@ class  Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome 8 hour of flutter by Rimon"),
-          ),
-        ),
-      ),
+
+
+
+      initialRoute: "/home",
+      routes: {
+        "/":(context)=>LoginPage(),
+        "/home":(context)=>HomePage(),
+        "/login":(context)=>LoginPage(),
+      },
     );
   }
 }
