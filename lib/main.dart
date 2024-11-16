6c1b28488eb528e026778fe7c2dd78e0e763c18a
miner <minerhossain1033@gmail.com>
2024-11-16 09:23:17 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codepur_project/pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'home_page.dart';
 
 void main()
@@ -16,9 +16,10 @@ class  Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-
-
+      theme: ThemeData(
+        fontFamily:GoogleFonts.lato().fontFamily,
+       primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       initialRoute: "/login",
       routes: {
         "/":(context)=>LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,10 +6,45 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.blue[50],
       child: Column(
+
         children: [
           // Display the image from the assets
-          Image.asset("assets/images/login.png"),
+          Image.asset("assets/images/login.png",fit:BoxFit.cover,),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text("Welcome to Login Page",style: TextStyle(fontSize:24,fontWeight:FontWeight.bold),),
+          SizedBox(
+            height: 20.0,
+          ),
+         Padding(
+           padding: const EdgeInsets.symmetric(vertical:16.0,horizontal:32.0),
+           child: Column(
+             children: [
+               TextFormField(
+                 decoration:InputDecoration(
+                   hintText: "Enter Username",
+                   labelText:"Username",
+                 ),
+               ),
+               TextFormField(
+                 obscureText: true,
+                 decoration:InputDecoration(
+                   hintText: "Enter Password",
+                   labelText:"Userpassword",
+                 ),
+               ),
+               SizedBox(
+                 height: 20.0,
+               ),
+               ElevatedButton(onPressed:(){}, child:Text("Login"),
+                 style:TextButton.styleFrom(),
+               )
+             ],
+           ),
+         )
         ],
       ),
     );
