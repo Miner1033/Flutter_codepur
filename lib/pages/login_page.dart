91aab0274196afa7792e8c0b416e02e7282e01bb
miner <minerhossain1033@gmail.com>
2024-11-16 09:56:05 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codepur_project/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,45 +8,51 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.blue[50],
-      child: Column(
-
-        children: [
-          // Display the image from the assets
-          Image.asset("assets/images/login.png",fit:BoxFit.cover,),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text("Welcome to Login Page",style: TextStyle(fontSize:24,fontWeight:FontWeight.bold),),
-          SizedBox(
-            height: 20.0,
-          ),
-         Padding(
-           padding: const EdgeInsets.symmetric(vertical:16.0,horizontal:32.0),
-           child: Column(
-             children: [
-               TextFormField(
-                 decoration:InputDecoration(
-                   hintText: "Enter Username",
-                   labelText:"Username",
+      child: SingleChildScrollView(
+        child: Column(
+        
+          children: [
+            // Display the image from the assets
+            Image.asset("assets/images/login.png",fit:BoxFit.cover,
+              height:300,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("Welcome to Login Page",style: TextStyle(fontSize:28,fontWeight:FontWeight.bold),),
+            SizedBox(
+              height: 20.0,
+            ),
+           Padding(
+             padding: const EdgeInsets.symmetric(vertical:16.0,horizontal:32.0),
+             child: Column(
+               children: [
+                 TextFormField(
+                   decoration:InputDecoration(
+                     hintText: "Enter Username",
+                     labelText:"Username",
+                   ),
                  ),
-               ),
-               TextFormField(
-                 obscureText: true,
-                 decoration:InputDecoration(
-                   hintText: "Enter Password",
-                   labelText:"Userpassword",
+                 TextFormField(
+                   obscureText: true,
+                   decoration:InputDecoration(
+                     hintText: "Enter Password",
+                     labelText:"Userpassword",
+                   ),
                  ),
-               ),
-               SizedBox(
-                 height: 20.0,
-               ),
-               ElevatedButton(onPressed:(){}, child:Text("Login"),
-                 style:TextButton.styleFrom(),
-               )
-             ],
-           ),
-         )
-        ],
+                 SizedBox(
+                   height: 40.0,
+                 ),
+                 ElevatedButton(onPressed:(){
+                   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                 }, child:Text("Login"),
+                   style:TextButton.styleFrom(minimumSize: Size(150,40)),
+                 )
+               ],
+             ),
+           )
+          ],
+        ),
       ),
     );
   }
